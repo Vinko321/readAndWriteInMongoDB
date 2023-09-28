1297f708f5dfb5113388c845f8e750a22e17e9be
Vinko321 <69211412+Vinko321@users.noreply.github.com>
2023-09-28 16:54:39 +0200

    app, env, package.json

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const indexRouter = require('./routes/index');
+const signupRouter = require('./routes/signup');
+const cureNajmladjeRouter = require('./routes/cureNajmladje');
+const igracRouter = require('./routes/igrac');
+/**
+ * dodjeljivanje puta statičkih datoteka (css, js style, images..)
+ */
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * prije dodjeljivanja bodyParser smo instalirali međusoftver bodyParser koji nam služi a privaćanje HTTP POST
+ */
+const bodyParser = require('body-parser');
+/**
+ * encoding tekstualni URL podataka (JSON) i izlaganje rezultata u req.Body
+ */
+app.use( bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); // set up ejs for templating
+/**
+ * kad klijent upiše u adresnu traku ovo između navodnika koristi rutu upisanu iza zareza
+ */
+app.use('/', indexRouter);
+app.use('/signup', signupRouter);
+app.use('/cureNajmladje', cureNajmladjeRouter);
+app.use('/igrac', igracRouter);
+
+
+
+app.listen(3000, () => {
+    console.log('Server listening on 3000');
+})
+
+
