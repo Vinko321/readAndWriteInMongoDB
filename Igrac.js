fad974ed74defbdaaa30f7e09dbf7614406078e0
Vinko321 <69211412+Vinko321@users.noreply.github.com>
2023-09-28 18:39:32 +0200

    model pages

---
+++ Igrac.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+let igracSchema = mongoose.Schema({
+    punoIme: {type: String, unique: true, required: true},
+    pozicija: {type: String, required: true},
+    uzrast: {type: String, required: true},
+    visina:{type: Number}
+});
+
+module.exports = mongoose.model('Igrac', igracSchema, 'Igraci');
